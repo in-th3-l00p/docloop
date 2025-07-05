3a215dcb35ea362cce438aa459d23d51fc6f9ada
Tisca Catalin <admin@tiscacatalin.com>
2025-07-05 09:58:14 +0300

    better button styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,8 +18,8 @@ export default function Button({
             type="button" 
             className={cn(
                 "cursor-pointer px-4 py-2 rounded-lg transition-all text-xs", 
-                variant === "primary" && "bg-secondary hover:bg-secondary/60",
-                variant === "secondary" && "bg-primary/20 hover:bg-primary/30",
+                variant === "primary" && "bg-secondary hover:bg-secondary/60 border border-primary/40",
+                variant === "secondary" && "bg-primary/10 hover:bg-primary/20 border border-primary/20",
                 className
             )}
             onClick={onClick}
