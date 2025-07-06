b36ec2f9ed6d0898c6e029841a5a682a04158a13
Tisca Catalin <admin@tiscacatalin.com>
2025-07-06 12:07:28 +0300

    working pdf generation

--- doc-maker/src/lib.rs
+++ doc-maker/src/lib.rs
@@ -1,17 +1,50 @@
 use wasm_bindgen::prelude::*;
 use js_sys::{Array, JsString};
-use printpdf::{Mm, PdfDocument, PdfPage, PdfSaveOptions};
+use printpdf::{PdfDocument, PdfSaveOptions, image::RawImage};
 use base64::{self, prelude::BASE64_STANDARD, Engine};
 
 #[wasm_bindgen]
-pub fn to_pdf(_images: Array) -> JsString {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn to_pdf(images: Array) -> JsString {
     let mut doc = PdfDocument::new("Hello world");
-    let page = PdfPage::new(Mm(210.0), Mm(297.0), vec![]);
     let mut warnings = Vec::new();
+
+    for image in images.iter() {
+        let image_base64 = match image.as_string() {
+            Some(image_base64) => image_base64,
+            None => {
+                error("error occured when getting image base64");
+                continue;
+            }
+        };
+        let image_base64 = image_base64
+            .split(";base64,")
+            .nth(1)
+            .unwrap()
+            .to_string();
+        let decoded = BASE64_STANDARD.decode(image_base64).unwrap();
+        let image = &RawImage::decode_from_bytes(&decoded, &mut warnings);
+        match image {
+            Ok(image) => {
+                doc.add_image(image);
+            }
+            Err(e) => {
+                error(&format!("error occured when decoding image: {:?}", e));
+                continue;
+            }
+        }
+    }
     
-    let doc_with_pages = doc.with_pages(vec![page]);
-    let pdf_bytes = doc_with_pages.save(&PdfSaveOptions::default(), &mut warnings);
-    
-    let base64_string = BASE64_STANDARD.encode(&pdf_bytes);
+    let base64_string = BASE64_STANDARD.encode(
+        doc.save(&PdfSaveOptions::default(), &mut warnings)
+    );
     JsString::from(base64_string)
 }
