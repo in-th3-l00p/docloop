0731e9247044af6c29ba6e6d2bf673bd136e0122
Tisca Catalin <admin@tiscacatalin.com>
2025-07-08 00:46:52 +0300

    dummy about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./routes/home";
+import About from "./routes/about";
 import Container from "./components/ui/container";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   }
 ]);
 
